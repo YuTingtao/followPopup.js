23aba2034df8f55131a0722e19d742e195753057
YuTingtao <1224100678@qq.com>
2018-11-03 19:28:13 +0800

    update

--- js/jquery.mousefollow.js
+++ js/jquery.mousefollow.js
@@ -1,114 +1,104 @@
 /*
- * mousefollow v0.1.0
+ * mousefollow v1.0.0
  * author 1224100678@qq.com
  * https://github.com/YuTingtao/mousefollow.js
  */
 ;(function($, window, document, undefined){
-    // 定义MouseFollow构造函数
-    var mouseFollow = function(el, opt){
-        this.el = el,
-        this.defaults = {
-            html: '',                // 插入的html
-            speed: 200,              // 淡出速度
-            x: 20,                   // 距离鼠标的水平距离
-            y: 20,                   // 距离鼠标的垂直距离
-            onEnter: function(e) {}, // 鼠标进入回调
-            onMove: function(e) {},  // 鼠标移动回调
-            onOut: function(e) {}    // 鼠标移除回调
-        },
-        this.vars = $.extend({}, this.defaults, opt),
-        this.init();
-    }
+	var methods = {
+		init: function(options) {
+			var defaults = {
+				html: '',                   // 插入的html
+				speed: 200,                 // 淡出速度
+				x: 20,                      // 距离鼠标的水平距离
+				y: 20,                      // 距离鼠标的垂直距离
+				onEnter: function(e) {}, // 鼠标进入回调
+				onMove: function(e) {},     // 鼠标移动回调
+				onOut: function(e) {}   // 鼠标移除回调
+			};
+			var settings = $.extend({}, defaults, options);
 
-    // 定义MouseFollow的方法
-    mouseFollow.prototype = {
-        // 插入DOM
-        appendDOM: function(){
-            if ($('.js-follow').length < 1) {
-                $('body').append('<div class="js-follow" style="position: fixed; top: 100%; display: none;"></div>');
-            }
-        },
+			return this.each(function() {
+				var $this = $(this);
+				var _html = settings.html,
+					_speed = settings.speed,
+					_x = settings.x,
+					_y = settings.y,
+					onEnter = settings.onEnter,
+					onMove = settings.onMove,
+					onOut = settings.onOut;
+				// 插入DOM
+				if ($('.js-follow').length < 1) {
+					$('body').append('<div class="js-follow" style="position: fixed; top: 100%; display: none;"></div>');
+				}
 
-        // 鼠标移入插入HTML
-        mouseEnter: function() {
-            var $this = this,
-                _class = this.vars.className,
-                _html = this.vars.html,
-                _speed = this.vars.speed;
-            $this.el.mouseenter(function(e) {
-                $('.js-follow').html(_html).fadeIn(_speed);
-                $this.vars.onEnter && $this.vars.onEnter.call(this,e);
-            });
-        },
+				// 鼠标移入插入HTML
+				$this.on('mouseenter', function(e) {
+					$('.js-follow').html(_html).fadeIn(_speed);
+					onEnter && onEnter.call(this,e);
+				});
 
-        // 鼠标移动改变位置
-        mouseMove: function() {
-            var $this = this,
-                _class = this.vars.className,
-                _html = this.vars.html,
-                _speed = this.vars.speed,
-                _x = this.vars.x,
-                _y = this.vars.y;
+				// 鼠标移动实时改变位置
+				$this.on('mousemove', function(e) {
+					e = e || window.e;
+					var x = e.clientX;
+					var y = e.clientY;
+					var setCssX = function() {
+						if ( x + _x + $('.js-follow').width() < $(window).width() ) {
+							$('.js-follow').css({
+								left: x + _x,
+								right: 'auto'
+							});
+						} else {
+							$('.js-follow').css({
+								left: 'auto',
+								right: $(window).width() - x + _x -10
+							});
+						}
+					}
+					var setCssY = function(){
+						if ( y + _y + $('.js-follow').height() < $(window).height() ) {
+							$('.js-follow').css({
+								top: y + _y,
+								bottom: 'auto'
+							});
+						} else {
+							$('.js-follow').css({
+								top: 'auto',
+								bottom: $(window).height() - y + _y - 20
+							});
+						}
+					}
+					setCssX();
+					setCssY();
+					onMove && onMove.call(this,e);
+				});
 
-            $this.el.mousemove(function(event) {
-                event = event || window.event;
-                var x = event.clientX;
-                var y = event.clientY;
-                var setCssX = function() {
-                    if ( x + _x + $('.js-follow').width() < $(window).width() ) {
-                        $('.js-follow').css({
-                            left: x + _x,
-                            right: 'auto'
-                        });
-                    } else {
-                        $('.js-follow').css({
-                            left: 'auto',
-                            right: $(window).width() - x + _x -10
-                        });
-                    }
-                }
-                var setCssY = function(){
-                    if ( y + _y + $('.js-follow').height() < $(window).height() ) {
-                        $('.js-follow').css({
-                            top: y + _y,
-                            bottom: 'auto'
-                        });
-                    } else {
-                        $('.js-follow').css({
-                            top: 'auto',
-                            bottom: $(window).height() - y + _y - 20
-                        });
-                    }
-                }
-                setCssX();
-                setCssY();
-                $this.vars.onMove && $this.vars.onMove.call(this,event);
-            });
-        },
+				// 鼠标移出隐藏DOM
+				$this.on('mouseleave', function(e) {
+					onOut && onOut.call(this,e);
+					$('.js-follow').hide();
+				});
 
-        // 鼠标移出删除
-        mouseOut: function() {
-            var $this = this,
-                _class = this.vars.className;
-            $this.el.mouseleave(function(e) {
-                $('.js-follow').hide();
-                $this.vars.onOut && $this.vars.onOut.call(this,e);
-            });
-        },
+			});
+		},
+		destroy: function() {
+			return $(this).each(function() {
+				var $this = $(this);
+				$('.js-follow').remove();
+				$this.off('mouseenter');
+				$this.off('mousemove');
+				$this.off('mouseleave');
+			});
+		}
+	};
 
-        // 初始化
-        init: function() {
-            this.appendDOM();
-            this.mouseEnter();
-            this.mouseMove();
-            this.mouseOut();
-        }
-    }
-
-    $.fn.mousefollow = function(opt){
-        // 创造MouseFollow实体
-        var mousefollow = new mouseFollow(this, opt);
-        // 调用其方法
-        return mousefollow;
-    }
+	$.fn.mousefollow = function(method) {
+		if (methods[method]) {
+			return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
+		} else if (typeof method === 'object' || !method) {
+			return methods.init.apply(this, arguments);
+		} else {
+			$.error('method ' + method + ' does not exist on jquery.mousemollow.js');
+		}
+	}
 })(jQuery, window, document);
