ba0f586191fdfa2389c77be5b50e78cfc0f7cbb7
YuTingtao <1224100678@qq.com>
2020-07-18 23:33:20 +0800

    Update jquery.followPopup2.js
    鼠标移动加入节流功能

--- dist/jquery.followPopup2.js
+++ dist/jquery.followPopup2.js
@@ -20,10 +20,11 @@
             fade: false,             // 是否淡入淡出:可传入int类型
             x: 20,                   // 距离鼠标的水平距离
             y: 20,                   // 距离鼠标的垂直距离
-            zIndex: 999,             // 插入html的层级   
-            afterEnter: function($this) {}, // 鼠标进入回调
-            onMove: function($this) {},     // 鼠标移动回调
-            beforeOut: function($this) {}   // 鼠标移除回调
+            zIndex: 999,             // 插入html的层级
+            throttleTime: 40,  // 节流时间间隔：鼠标移动时该段时间内只触发一次执行程序
+            afterEnter: function() {}, // 鼠标进入回调
+            onMove: function() {},     // 鼠标移动回调
+            beforeOut: function() {}   // 鼠标移除回调
         };
         this.opt = $.extend({}, this.defaults, options);
         this.init();
@@ -61,36 +62,43 @@
                 _html = this.opt.html,
                 _speed = this.opt.speed,
                 _x = this.opt.x,
-                _y = this.opt.y;
+                _y = this.opt.y,
+                _throttleTime = this.opt.throttleTime;
 
             $this.el.mousemove(function(e) {
                 e = e || window.e;
-                var x = e.clientX;
-                var y = e.clientY;
-                setTimeout(function() {
-                    if ( x + _x + $('.js-followPopup').width() < $(window).width() ) {
-                        $('.js-followPopup').css({
-                            left: x + _x,
-                            right: 'auto'
-                        });
-                    } else {
-                        $('.js-followPopup').css({
-                            left: 'auto',
-                            right: $(window).width() - x + _x -10
-                        });
-                    }
-                    if ( y + _y + $('.js-followPopup').height() < $(window).height() ) {
-                        $('.js-followPopup').css({
-                            top: y + _y,
-                            bottom: 'auto'
-                        });
-                    } else {
-                        $('.js-followPopup').css({
-                            top: 'auto',
-                            bottom: $(window).height() - y + _y - 20
-                        });
-                    }
-                }, 0);
+                var x = parseInt(e.clientX);
+                var y = parseInt(e.clientY);
+
+                var timeout = null;
+                if (!timeout) {
+                    setTimeout(function() {
+                        if ( x + _x + $('.js-followPopup').width() < $(window).width() ) {
+                            $('.js-followPopup').css({
+                                left: x + _x,
+                                right: 'auto'
+                            });
+                        } else {
+                            $('.js-followPopup').css({
+                                left: 'auto',
+                                right: $(window).width() - x + _x -10
+                            });
+                        }
+                        if ( y + _y + $('.js-followPopup').height() < $(window).height() ) {
+                            $('.js-followPopup').css({
+                                top: y + _y,
+                                bottom: 'auto'
+                            });
+                        } else {
+                            $('.js-followPopup').css({
+                                top: 'auto',
+                                bottom: $(window).height() - y + _y - 20
+                            });
+                        }
+                    }, _throttleTime);
+                    timeout = null;
+                }
+
                 $this.opt.onMove && $this.opt.onMove.call(this, $this.el);
             });
         },
@@ -114,12 +122,10 @@
     }
 
     $.fn.followPopup = function(opt) {
-        // 创造followPopup实例
+        // 返回followPopup实例
         this.each(function() {
             var $this = $(this);
-            var followPopup = new FollowPopup($this, opt);
-            // 调用其方法
-            return followPopup;
+            return new FollowPopup($this, opt);
         });
     }
 }));
